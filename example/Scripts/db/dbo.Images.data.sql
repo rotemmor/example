3974266ce9105e5b9b4388c349d86561ef5175ca
קובי <@KOBY>
2016-09-13 17:18:19 +0300

    sdada

---
+++ example/Scripts/db/dbo.Images.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[Images] ON
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (7, N'rotem', N'mor', 500, N'asdasdas')
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (22, N'https://images.google.com/', N'1.png', 8580, N'2015-03-03T08:21:04.457Z')
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (23, N'https://images.google.com/', N'955985-hd-quality-beach-image.jpg', 7367601, N'2016-05-30T21:40:41.567Z')
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (24, N'https://images.google.com/', N'Beautiful-Apple-Nature-HD-Wallpaper.jpg', 1126986, N'2016-07-03T21:47:28.080Z')
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (25, N'https://images.google.com/', N'2.jpg', 4740, N'2016-03-20T09:15:31.619Z')
+INSERT INTO [dbo].[Images] ([Id], [imageUrl], [imageName], [ImageSize], [imageModifiedDate]) VALUES (26, N'https://images.google.com/', N'koby.jpg', 4740, N'2016-03-20T09:15:31.619Z')
+SET IDENTITY_INSERT [dbo].[Images] OFF
